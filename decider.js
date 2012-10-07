829b0f23613e20398886fee5ba176116e4736356
Martin de Keijzer <martin.dekeijzer@gmail.com>
2012-10-07 12:50:02 +0200

    jshint and jslint proof

--- decider.js
+++ decider.js
@@ -1,13 +1,14 @@
-      var decisionFormEl = document.getElementById('decisionForm');
-      
-      decisionFormEl.addEventListener('submit', function(event){
-        event.preventDefault();
+(function () {
+    "use strict";
+    var decisionFormEl = document.getElementById('decisionForm');
 
+    decisionFormEl.addEventListener('submit', function (event) {
         var decisionEl = document.getElementById('decision'),
             numChoices = document.getElementById('availableChoices').value,
             choice = Math.floor((Math.random() * numChoices) + 1);
-
+        event.preventDefault();
         decisionEl.innerHTML = "We think you should pick number: " + choice;
         decisionEl.style.display = "block";
-      });
+    });
+}());
 
